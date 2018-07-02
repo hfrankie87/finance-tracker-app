aa08fd1093b07e8d4636209d64a1c273a84d1137
Frank Hernandez <hfrank0714@gmail.com>
2018-07-02 07:08:02 +0000

    added search actions and errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,10 +3,20 @@ class UsersController < ApplicationController
     def my_portfolio
        @user = current_user
        @user_stocks = current_user.stocks
-   end
+    end
    
-   def my_friends
-       
-   end
+    def my_friends
+      @friendships = current_user.friends  
+    end
+    
+    def search
+        if params[:search_param].blank?
+         flash.now[:danger] = "You have entered an empty search string"
+      else
+         @users = User.search(params[:search_param])
+         flash.now[:danger] = "No users match this search criteria" if @users.blank?
+        end
+         render partial: 'friends/result'
+    end
 end
    
